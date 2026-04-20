d506de2567b9107b92afdf14618f9b35c66b4ce3
SAHIL S. CHAPLE <sahilchaple96@gmail.com>
2026-04-20 17:54:19 +0530

    Add files via upload

--- songs-data.js
+++ songs-data.js
@@ -12,7 +12,7 @@ export const songs = [
     artist: "Asha Bhosle, R.D. Burman",
     albumId: 1,
     duration: "2:35",
-    cover: "images/Dhurandhar-title-track.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687172/Dhurandhar-title-track_wolv0w.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597534/Dhurandhar_-_Title_Track_-_Dhurandhar_320_kbps_wiemwg.mp3" // Placeholder Cloudinary URL
   },
   {
@@ -21,7 +21,7 @@ export const songs = [
     artist: "Asha Bhosle",
     albumId: 1,
     duration: "4:13",
-    cover: "images/lutt-le-gaya.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687171/Lutt-Le-Gaya_qwihoh.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597562/Lutt_Le_Gaya_ritd1l.mp3"
   },
   {
@@ -30,7 +30,7 @@ export const songs = [
     artist: "Suresh Wadkar",
     albumId: 1,
     duration: "2:35",
-    cover: "images/naal-nachna.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687173/Naal-Nachna_mkbvt4.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597553/Naal_Nachna_-_Dhurandhar_320_kbps_llqz2v.mp3"
   },
   {
@@ -39,7 +39,7 @@ export const songs = [
     artist: "Asha Bhosle, Usha Uthup",
     albumId: 1,
     duration: "2:41",
-    cover: "images/ramba-ho.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687172/ramba-ho_chmljv.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597559/Ramba_Ho_-_Dhurandhar_320_kbps_gvmnol.mp3"
   },
   {
@@ -48,7 +48,7 @@ export const songs = [
     artist: "Monica",
     albumId: 1,
     duration: "2:20",
-    cover: "images/run-down-the-city.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687171/run-down-the-city_cui2hi.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597552/Run_Down_The_City_-_Monica_-_Dhurandhar_320_kbps_din1n7.mp3"
   },
   {
@@ -57,7 +57,7 @@ export const songs = [
     artist: "Asha Bhosle",
     albumId: 1,
     duration: "3:44",
-    cover: "images/shararat.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687171/shararat_tbsblq.jpg",
     file: "https://res.cloudinary.com/dbfdls6ub/video/upload/v1776597557/Shararat_-_Dhurandhar_320_kbps_y7i3hl.mp3"
   }
 ];
@@ -67,28 +67,7 @@ export const albums = [
     id: 1,
     title: "Dhurandhar (1983)",
     artist: "R. D. Burman",
-    cover: "images/Dhurandhar-title-track.jpg",
+    cover: "https://res.cloudinary.com/dbfdls6ub/image/upload/v1776687172/Dhurandhar-title-track_wolv0w.jpg",
     description: "The classic Bollywood energetic soundtrack composed by the legend R. D. Burman."
-  },
-  {
-    id: 2,
-    title: "Future Retro",
-    artist: "Neon Collective",
-    cover: "https://placehold.co/400x400/2a1e3c/ff00ff?text=Future+Retro",
-    description: "The best of synthwave and electronic."
-  },
-  {
-    id: 3,
-    title: "Unplugged Sessions",
-    artist: "Wood & Strings",
-    cover: "https://placehold.co/400x400/3e2c1e/d4af37?text=Acoustic+Hits",
-    description: "Pure acoustic melodies."
-  },
-  {
-    id: 4,
-    title: "Ambient Space",
-    artist: "Orbit",
-    cover: "https://placehold.co/400x400/0a0a1a/7aaae0?text=Ambient+Space",
-    description: "Atmospheric sounds from the void."
   }
 ];
